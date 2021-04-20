34f2c14d8b92c8f95f3955165c2fbf88b0392bf7
EstebanP <epinczinger@gmail.com>
2021-04-19 23:38:10 -0300

    add getCoin action

---
+++ src/actions/index.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const API_KEY = 'coinrankinge61e17811f108b21fb2a25fab66481336fe4e17c817aa19d';
+const API_URL = 'https://api.coinranking.com/v2';
+
+export const getCoinData = () => (dispatch) => {
+  axios({
+    method: 'get',
+    url: `${API_URL}/coin/Qwsogvtv82FCd`,
+    headers: {
+      Accept: 'application/json',
+      Authorization: API_KEY,
+    },
+  })
+    .then((response) => {
+      console.log(response);
+      dispatch({
+        type: 'GET_COINS',
+        payload: response,
+      });
+    })
+    .catch((error) => dispatch({
+      type: 'GET_COINS_ERROR',
+      payload: error,
+    }));
+};
+
+export const ask = () => {};
