18cbee7587d259d539071c2eba1ea37f27a52269
EstebanP <epinczinger@gmail.com>
2021-04-20 15:32:49 -0300

    change api url

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,14 @@
 import axios from 'axios';
-
-const API_KEY = 'coinrankinge61e17811f108b21fb2a25fab66481336fe4e17c817aa19d';
-const API_URL = 'https://api.coinranking.com/v2';
+const API_URL = 'api.coincap.io/v2/assets';
 
 export const getCoinData = () => (dispatch) => {
   axios({
     method: 'get',
-    url: `${API_URL}/coin/Qwsogvtv82FCd`,
+    url: API_URL,
     headers: {
       Accept: 'application/json',
-      Authorization: API_KEY,
+      Accept_Encoding: 'gzip',
+      mode: 'cors',
     },
   })
     .then((response) => {
