32b47e9fcb0c7025b6cf6719437eb296fc5e0086
EstebanP <epinczinger@gmail.com>
2021-04-21 13:35:23 -0300

    add shadow on navbar, delete unused code on app

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import Header from './Header';
 import Footer from './Footer';
 import CoinList from './CoinList';
@@ -7,11 +7,7 @@ import { getCoinData } from '../actions';
 
 const App = () => {
   // const coins = useSelector((state) => state.coin);
-  const theme = useSelector((state) => state.toggle);
   const dispatch = useDispatch();
-  const askData = () => {
-    dispatch(getCoinData());
-  };
 
   useEffect(() => {
     dispatch(getCoinData());
@@ -21,15 +17,6 @@ const App = () => {
     <div>
       <Header />
       <CoinList />
-
-      <button
-        className={theme}
-        name="button"
-        onClick={() => { askData(); }}
-        type="button"
-      >
-        Button
-      </button>
       <Footer />
     </div>
   );
