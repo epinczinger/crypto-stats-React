c1b129711ad28e120b80633f5185a59a263c1f4c
EstebanP <epinczinger@gmail.com>
2021-04-23 18:06:22 -0300

    delete unused files

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link, BrowserRouter as Router } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Coin = ({
   id, symbol, name, price, change,
@@ -12,28 +12,26 @@ const Coin = ({
       id={id}
       className={`border bg-gray-200 dark:bg-gray-800 ${id} hover:border-yellow-600 hover:bg-gray-300 dark:hover:bg-gray-700 shadow mx-auto sm:w-full text-center w-7/8 flex flex-col sm:flex-row justify-between`}
     >
-      <Router>
-        <div className="p-2 text-lg sm:w-1/5">
-          <Link to={`/coin/${id}`}>{symbol}</Link>
-        </div>
-        <div className="p-2 sm:w-1/5 text-xl">
-          <p>{name}</p>
-        </div>
-        <div className="p-2 sm:w-1/5">
-          <p className="hidden sm:block">{`$ ${price}`}</p>
-          <p className="sm:hidden text-xl">{`$ ${price}`}</p>
-        </div>
-        <div className={`${greenRed} p-2 font-semibold sm:w-1/5`}>
-          <p>{`${change} %`}</p>
-        </div>
-        <div className="p-2 sm:w-1/5 text-center">
-          <Link to={`/coin/${id}`}>
-            <p className="rounded-full mx-auto h-7 w-7 hover:text-gray-800 text-semibold border border-gray-600 flex items-center justify-center">
-              +
-            </p>
-          </Link>
-        </div>
-      </Router>
+      <div className="p-2 text-lg sm:w-1/5">
+        <Link to={`/coin/${id}`}>{symbol}</Link>
+      </div>
+      <div className="p-2 sm:w-1/5 text-xl">
+        <p>{name}</p>
+      </div>
+      <div className="p-2 sm:w-1/5">
+        <p className="hidden sm:block">{`$ ${price}`}</p>
+        <p className="sm:hidden text-xl">{`$ ${price}`}</p>
+      </div>
+      <div className={`${greenRed} p-2 font-semibold sm:w-1/5`}>
+        <p>{`${change} %`}</p>
+      </div>
+      <div className="p-2 sm:w-1/5 text-center">
+        <Link to={`/coin/${id}`}>
+          <p className="rounded-full mx-auto h-7 w-7 hover:text-gray-800 text-semibold border border-gray-600 flex items-center justify-center">
+            +
+          </p>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/pages/__test__/AboutUs.spec.js
+++ src/pages/__test__/AboutUs.spec.js
@@ -1,9 +1,9 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import AboutUs from "../AboutUs";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import AboutUs from '../AboutUs';
 
-describe("The AboutUs component", () => {
-  it("renders as expected matching snapshot", () => {
+describe('The AboutUs component', () => {
+  it('renders as expected matching snapshot', () => {
     const tree = renderer.create(<AboutUs />).toJSON();
     expect(tree).toMatchSnapshot();
   });
