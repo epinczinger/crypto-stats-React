93362221a0c512502324cc6d7259d1e3f4295613
EstebanP <epinczinger@gmail.com>
2021-04-25 11:55:12 -0300

    re ordering files on containers structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import DefaultNotFound from './pages/DefaultNotFound';
+import DefaultNotFound from './components/DefaultNotFound';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Main from './pages/Main';
+import Main from './containers/Main';
 import { getCoinData } from './actions';
-import CoinDetails from './pages/CoinDetails';
-import AboutUs from './pages/AboutUs';
+import CoinDetails from './containers/CoinDetails';
+import AboutUs from './components/AboutUs';
 
 const App = () => {
   const dispatch = useDispatch();
